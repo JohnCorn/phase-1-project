8f3a65b1543fc1fe646b5a24b7f78d49443b97ac
John Corn <jcornacchioli@gmail.com>
2023-06-23 10:46:22 -0400

    outline html

--- index.js
+++ index.js
@@ -5,3 +5,13 @@
 • Click on a line to see all the stations
 • Click on a station to see the times
 */
+
+window.addEventListener("DOMContentLoaded", (event) => {
+    console.log("DOM fully loaded and parsed");
+    GetDisplaySubwayLines();
+  });
+
+  function GetDisplaySubwayLines()
+  {
+    console.log("GetDisplaySubwayLines");
+  }
