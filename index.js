66de13b9b79423073236329689ec14334393ef4b
John Corn <jcornacchioli@gmail.com>
2023-06-24 15:27:36 -0400

    got tailwind up and running

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
-/*
-=== TODO ===
-• Pull in date from MTA api
-• Display each subway line
-• Click on a line to see all the stations
-• Click on a station to see the times
-*/
-
 let fetchedData;
 let burgerInfo;
 let episodeInfo;
+let dropdownIsOpen = false;
+
 
 //#region Initialization
+const handleEscape = e => {
+    if (e.key ==='Esc'|| e.key === 'Escape')
+        ToggleDropDown(false);
+}
+
 window.addEventListener("DOMContentLoaded", (event) => 
 {    
-    const dropdown = document.getElementById('dropdown');
-    dropdown.addEventListener('change', event => DropDownChange(event.target.value));
+    document.getElementById('dropdownBtn').addEventListener('click', event => {
+        ToggleDropDown(!dropdownIsOpen);
+    });
+
+    document.addEventListener('keydown', handleEscape);
 
     burgerInfo  = document.getElementById('burgerInfo');
     episodeInfo = document.getElementById('episodeInfo');
-    GetAllBurgers(dropdown);
+    GetAllBurgers();
   });
 
-function GetAllBurgers(dropdown)
+function GetAllBurgers()
 {
     let url = 'https://bobsburgers-api.herokuapp.com/burgerOfTheDay/';
     
@@ -40,16 +42,22 @@ function GetAllBurgers(dropdown)
         // Loop though all the data and pull out the burger name and add it to the drop down
         for(let i = 0; i < fetchedData.length; i++)
         {
-            let option = document.createElement('option');
-            option.value = i;
+            const ddMenu = document.getElementById('ddMenu');
 
+            let option = document.createElement('a');
+            // <a href="#" class="block px-4 py-1 text-gray-800 hover:bg-indigo-500 hover:text-white">Test 1</a>
+            option.classList = "block px-4 py-1 text-gray-800 hover:bg-indigo-500 hover:text-white";
+            option.href = "#";
             let displayText = SplitName(data[i].name);
+
             option.innerText = displayText[0];
+            ddMenu.append(option);
 
-            dropdown.append(option);
+            option.addEventListener('click', event => {
+                DropDownChange(i);
+                ToggleDropDown(false);
+            });
         }
-
-        DropDownChange(0);
     });
 }
 //#endregion
@@ -131,4 +139,22 @@ function SplitName(fullName)
         nameArray.push(splitName[1].toLowerCase());
 
     return nameArray;
+}
+
+function ToggleDropDown(isOpen)
+{
+    // dont toggle drop down if current state is called
+    if (isOpen == dropdownIsOpen)
+        return;
+
+    dropdownIsOpen = isOpen;
+    console.log(`dropDownIsOpen: `, isOpen);
+
+    let ddMenu = document.getElementById('ddMenu');
+    if(ddMenu.classList.contains('hidden'))
+    {
+        ddMenu.classList.remove('hidden');
+    }else{
+        ddMenu.classList.add('hidden');
+    }
 }
