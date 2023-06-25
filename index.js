4255b6ff406ba66ffd82322646444a58ca39a284
John Corn <jcornacchioli@gmail.com>
2023-06-25 09:25:15 -0400

    added pokemon type and cleaned up code

--- index.js
+++ index.js
@@ -1,99 +1,25 @@
 let pokemonData = [];
 
-window.addEventListener("DOMContentLoaded", (event) => {
-    //console.log("DOM fully loaded and parsed");
-    SetUp();
+// TODO: have a solid read me
+// - look for all the bugs, break it in the browser
+window.addEventListener("DOMContentLoaded", (event) => 
+{
+    let a = GetPokemonData()
+    .then(res => {
+      pokemonData = res;
+      DisplayCards();
+    });
 
     document.getElementById('closeBtn').addEventListener('click', event => ToggleSlide(false));
-
   });
 
-  function SetUp()
-  {
-    // TODO: add notes
-    let urls = GetSetAllPokemonURL()
-    .then(res => {
-        let a = GetPokemonData(res)
-        .then(data => {
-          pokemonData = data;
-          DisplayCards(pokemonData)
-        });
-    })
-  }
-
-  function DisplayCards(pokeData)
-  {
-    const cardLayout = document.getElementById('CardLayout');
-
-    for(let i = 0; i < pokeData.length; i++)
-    {
-        // create div to hold the card
-        const cardHolder = document.createElement('div');
-        cardHolder.classList ="p-6 rounded-3xl border-4 border-white bg-neutral-100 flex gap-5 items-center shadow-xl"
-        + " hover:shadow-xl hover:shadow-black transition-shadow duration-250 ease-out"
-        + " hover:scale-105 transition-scale duration-250  ease-out cursor-pointer";
-
-        const card = document.createElement('div');
-        card.classList ="relative flex justify-center";
-        cardHolder.append(card);       
-
-        // TODO: might want to off load this to another method???
-        // create background for portait
-        const portraitHolder = document.createElement('div');
-        portraitHolder.classList = 'h-16 w-16 overflow-hidden rounded-full bg-black flex items-center justify-center'
-
-        // create portrait image 
-        const portraitImage = document.createElement('img');
-        portraitImage.src = pokeData[i].sprites.front_default;
-        portraitImage.classList = 'h-9/10 w-9/10 object-cover';
-
-        portraitHolder.append(portraitImage);
-        card.append(portraitHolder);
-
-        // create name header
-        const nameHolder = document.createElement('div');
-        nameHolder.className = 'rounded-full'
-        nameHolder.style = 'background-color: #194c89;'
-
-        const nameHeader = document.createElement('h1');
-        nameHeader.textContent = pokeData[i].name;
-
-        card.append(nameHolder);
-        nameHolder.append(nameHeader);
-        
-        // add event listner for when it's clicked
-        card.addEventListener('click', event => {
-          SlideCardSetUp(i);
-          ToggleSlide(true)
-        });
-
-        cardLayout.append(cardHolder);
-    }
-  }
-  
-  async function GetSetAllPokemonURL()
-  {
-    let a = await fetch(`https://pokeapi.co/api/v2/pokemon/`,
-        {
-        method: "GET",
-        header: {
-          "Content-Type": "application/json",
-        },
-        })
-        .then(res => res.json())
-        .then (data => {
-            return data
-        });
-
-        return a;
-  }
-
-  async function GetPokemonData(pokemon)
+  async function GetPokemonData()
   {
     let p =[];
-    for(let i = 0; i < pokemon.results.length; i++)
+    // TODO: refactor this so it uses and array method
+    for(let i = 1; i <= 20; i++)
     {
-        let a = await fetch(pokemon.results[i].url,
+        let a = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}/`,
         {
         method: "GET",
         header: {
@@ -109,16 +35,163 @@ window.addEventListener("DOMContentLoaded", (event) => {
     return p;
   }
 
-  function SlideCardSetUp(index)
+  //#region static card display
+  function DisplayCards()
   {
-    console.log(`setting up:`, pokemonData[index].name);
+    const cardLayout = document.getElementById('CardLayout');
 
+    for(let i = 0; i < pokemonData.length; i++)
+    {
+        const cardInfo = CreateCardGroup(cardLayout, i);
 
+        // create and setups up background and image for portait
+        const portraitHolder =  CreatePortraitHolder(pokemonData[i]);    
+        cardInfo.append(portraitHolder);
+
+        // create name header
+        const namePlate = CreateNamePlate(pokemonData[i]);
+        cardInfo.append(namePlate);
+    }
+  }
+
+  function CreateCardGroup(cardLayout, index)
+  {
+    // create div to hold the card
+    const cardHolder = document.createElement('div');
+    cardLayout.append(cardHolder);
+
+    const card = document.createElement('div');
+    //card.classList ="relative flex justify-center";
+    card.classList =
+    "p-6 rounded-3xl border-4 border-white bg-neutral-100 flex gap-5 items-center shadow-xl"
+    + " hover:shadow-xl hover:shadow-black transition-shadow duration-250 ease-out"
+    + " cursor-pointer hover:p-4"
+    + " hover:-translate-y-2 hover:border-8 hover:border-orange-300 hover:bg-yellow-100 ";
+    cardHolder.append(card);    
+    
+    // add event listner for when it's clicked
+    card.addEventListener('click', event => {
+      PopOutCardSetUp(index);
+      ToggleSlide(true)
+    });
+
+    const cardInfo = document.createElement('div');
+    cardInfo.classList ="relative flex justify-center";
+    card.append(cardInfo);
+
+    return cardInfo;
+  }
+  
+  function CreateNamePlate(pokeData)
+  {
+    const nameHolder = document.createElement('div');
+    nameHolder.classList ='object-contain';
+
+    const nameBackground = document.createElement('div');
+    nameBackground.classList = 'bg-gray-500 text-white font-semibold rounded-full px-3 py-0.5'
+    //nameBackground.style = 'background-color: #194c89;'
+    nameHolder.append(nameBackground);
+
+    const nameHeader = document.createElement('span');
+    nameHeader.textContent = pokeData.species.name;
+    nameBackground.append(nameHeader);
+
+    return nameHolder;
+  }
+
+  // TODO: find a better name for the parameter
+  function CreatePortraitHolder(pokeData)
+  {
+    const portraitHolder = document.createElement('div');
+    portraitHolder.classList = 'h-16 w-16 overflow-hidden rounded-full bg-gray-300 flex items-center justify-center'
+
+    // create portrait image 
+    const portraitImage = document.createElement('img');
+    portraitImage.src = pokeData.sprites.front_default;
+    portraitImage.classList = 'h-9/10 w-9/10 object-cover';
+
+    portraitHolder.append(portraitImage);
+
+    return portraitHolder;
+  }
+  //#endregion
+
+  //#region  PopOut
+  function PopOutCardSetUp(index)
+  {
     document.getElementById('innerName').textContent = pokemonData[index].name;
+    document.getElementById('innerPortrait').src = pokemonData[index].sprites.front_default;  
+    
+    const typeHolder = document.getElementById('TypeHolder');
 
-    document.getElementById('innerPortrait').src = pokemonData[index].sprites.front_default;
-   
+    // clear out old values
+    typeHolder.innerHTML ='';
+
+    for(let i = 0; i < pokemonData[index].types.length; i++)
+    {
+      let typeName = pokemonData[index].types[i].type.name;
+
+      const holder = document.createElement('div');
+      const typeColor = GetTypeColorString(typeName);
+
+      holder.classList =`flex justify-center rounded-full text-white ${typeColor} font-semibold`;
+      const typeText = document.createElement('span');
+      typeText.innerText = pokemonData[index].types[i].type.name;
+
+      holder.append(typeText);
+      typeHolder.append(holder);
+    }
+
+    // Move sets
+    const moveHolder = document.getElementById('moveHolder');
+    for(let i = 0; i < pokemonData[index].moves.length; i++)
+    {
+      console.log();
+    }
+    const portraitHolder = document.createElement('span');
+    
+    //TODO: add getset color method for pokemon type: ex: grass returns green
     // TODO: add nect and last buttons
+  }
+
+  function GetTypeColorString(typeString)
+  {
+    let colorString = "bg-gray-500"
+    switch(typeString)
+    {
+      case('normal'):
+      colorString = "bg-yellow-500";
+      break;
+
+      case('fire'):
+      colorString = "bg-orange-500";
+      break;
+
+      case('water'):
+      colorString = "bg-blue-500";
+      break;
+
+      case('grass'):
+      colorString = "bg-green-500";
+      break;
+     
+      case('poison'):
+      colorString = "bg-purple-500";
+      break;
+
+      case('flying'):
+      colorString = "bg-rose-500";
+      break;
+
+      case('bug'):
+      colorString = "bg-lime-500";
+      break;
+
+      default:
+        break;
+    }
+
+    return colorString;
   }
 
   function ToggleSlide(isOpen)
@@ -133,3 +206,4 @@ window.addEventListener("DOMContentLoaded", (event) => {
         document.getElementById('slideCard').classList.remove('hidden');
     }
   }
+  //#endregion
